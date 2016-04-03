db28c78bb918cc671c950807be84ca1c11fb6df0
paco <franmenendezpouso@gmail.com>
2016-04-03 18:10:09 +0200

    clases ArrayListInt y Alumno

---
+++ Alumno.java
@@ -0,0 +1,84 @@
+
+/**
+ * Write a description of class Alumno here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alumno
+{
+    // Nota minima de aprobado
+    public static final int NOTA_APROBADO = 5;
+    // Numero de alumnos en clase, para asignar
+    public static int numAlumnosClase = 1;
+
+    // Nombre del alumno
+    private String nombre;
+    // Edad del alumno
+    private int edad;
+    // Numero de clase
+    private int numeroClase;
+    // Conjunto de notas del alumno
+    private ArrayListInt notas;
+
+    /**
+     * Constructor for objects of class Alumno
+     * @param Nombre El nombre del alumno
+     * @param Edad La edad del alumno
+     */
+    public Alumno(String nombre, int edad)
+    {
+        // Inicializamos los atributos
+        this.nombre = nombre;
+        this.edad = edad;
+        numeroClase = numAlumnosClase;
+        // Aumentamos el numero de alumnos en clase
+        numAlumnosClase += 1;
+        notas = new ArrayListInt();
+    }
+
+    /**
+     * Añade notas al alumno
+     * @param Nota Nota a añadir
+     */
+    public void addNota(int nota)
+    {
+        notas.add(nota);
+    }
+
+    /**
+     * Devuelve la media de las notas del alumno
+     * @return La media de las notas del alumno
+     */
+    public int notaMedia()
+    {
+        int sumatorio = 0;
+        // Sumamos todos los elementos del array
+        for(int i = 0; i < notas.size(); i++){
+            sumatorio += notas.get(i);
+        }
+        // Devolvemos la media
+        return (sumatorio/notas.size());
+    }
+    
+    /**
+     * Devuelve si el alumno esta o no aprobado
+     * @return True si esta aprobado, false si no
+     */
+    public boolean aprobado()
+    {
+        return (notaMedia() >= NOTA_APROBADO);
+    }
+    
+    /**
+     * Imprime por pantalla la información del alumno
+     * @return Una string con la información del alumno
+     */
+    public String toString()
+    {
+        String info = "Nombre: " + nombre + "\nEdad: " + edad + "\nNumero de clase: " + numeroClase +  "\nNota media: " + notaMedia() + " Aprobado: " + aprobado();
+        return info;
+    }
+
+}
+
